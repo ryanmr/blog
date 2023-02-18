2aaa0a24fa6adc5b617f805ad366f0ae5c02ea22
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-02-17 21:04:17 -0600

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    mdx(),
+    mdx({
+      remarkPlugins: [remarkToc],
+      rehypePlugins: [
+        rehypeHeadingIds,
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: "wrap" }],
+      ],
+    }),
     react(),
     tailwind({ config: { applyBaseStyles: false } }),
   ],
