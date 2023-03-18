a6b580b7c8122a524d7c4f40006254db702c92df
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-03-18 10:50:27 -0400

    Upgrades to Astro 2.x

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,4 @@
 import rss from "@astrojs/rss";
-import sanitizeHtml from "sanitize-html";
 
 const postImportResult = import.meta.glob("./posts/*.mdx", { eager: true });
 const posts = Object.values(postImportResult);
@@ -7,6 +6,13 @@ const posts = Object.values(postImportResult);
 // https://github.com/withastro/rfcs/discussions/419
 
 export const get = async () => {
+  const items = posts.map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    description: post.frontmatter.description,
+    pubDate: post.frontmatter.publishDate,
+  }));
+
   return rss({
     // `<title>` field in output xml
     title: "ryanmr.com blog",
@@ -18,13 +24,7 @@ export const get = async () => {
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
-    items: posts.map((post) => ({
-      link: post.url,
-      title: post.frontmatter.title,
-      description: post.frontmatter.description,
-      pubDate: post.frontmatter.pubDate,
-      content: post.Content,
-    })),
+    items,
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
