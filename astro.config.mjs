89fc57df248b3bbbb37d59d7dbcfc5d43c1a61d2
Ryan Rampersad <ryan.rampersad@gmail.com>
2023-07-06 18:31:01 -0500

    Comments config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,39 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    //
+    // enables mdx
+    //
     mdx(),
+
+    ///
+    // enables react support in the rare case its useful
+    //
     react(),
-    tailwind({ config: { applyBaseStyles: false } }),
+
+    //
+    // enables integrated tailwind support
+    ///
+    tailwind({
+      //
+      // disables default tailwind base styles
+      // and instead configure it in the ./src/styles/tailwind.css
+      ///
+      applyBaseStyles: false,
+    }),
+
+    // inlines critical css
+    // as a better alternative to build.inlineStylesheets='auto'
+    critters(),
   ],
+
+  //
+  // where the site lives upon deployment
+  //
   site: `http://www.ryanmr.com`,
 });
